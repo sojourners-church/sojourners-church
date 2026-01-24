5dcbc5efb4c6fe53eeacfde8779e018d005dfb4d
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2026-01-24 08:12:38 -0600

    fix: remove support for unused markdown processing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,12 @@
 // deploy adapter
 import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import reactCompilerPlugin from "babel-plugin-react-compiler";
 // md parsing
 import remarkDirective from "remark-directive";
-
-// eslint-disable-next-line no-restricted-imports
-import { remarkDirectiveToHTML } from "./src/lib/remark-directive-to-html";
-
-import sitemap from "@astrojs/sitemap";
 
 const SITE_URL =
   process.env.CONTEXT === "production"
@@ -19,18 +15,18 @@ const SITE_URL =
     : process.env.DEPLOY_PRIME_URL;
 
 export default defineConfig({
-  markdown: {
-    remarkPlugins: [remarkDirective, remarkDirectiveToHTML],
-  },
   site: SITE_URL ?? "http://localhost:4321",
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react({
-    babel: {
-      plugins: [reactCompilerPlugin],
-    },
-  }), sitemap()],
+  integrations: [
+    react({
+      babel: {
+        plugins: [reactCompilerPlugin],
+      },
+    }),
+    sitemap(),
+  ],
   adapter: netlify(),
 });
